9c9850fdec50f238672e88872e40b4760edec004
Rafis Bagautdinov <rafisbb@gmail.com>
2018-08-07 00:32:07 +0500

    Divider of URL string is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  sharedText = '';
+  sharedCount = '2';
+  resultArray: Array<string> = [];
+
+  shareSubmit(): void {
+    this.resultArray = [];
+    const sharedCount = +this.sharedCount;
+    const linkLength: number = this.sharedText.length;
+
+    for (let i = 0; i < sharedCount; i++) {
+      let itemResult = '';
+
+      for (let j = 0; j < linkLength; j++) {
+        if (j === 0) {
+          itemResult += this.sharedText[i] || '';
+          continue;
+        }
+
+        itemResult += this.sharedText[i + j * sharedCount] || '';
+      }
+
+      this.resultArray.push(itemResult);
+    }
+  }
+
+  /*copy() {
+    const textarea = document.getElementById('textarea');
+    textarea.select();
+    document.execCommand('copy');
+  }*/
 }
